1290c2eddaec466180c8f2809a9c9d10295b0036
chmaha <reaclassical@gmail.com>
2025-03-22 14:34:01 +0000

    Improve look of build script

--- build.sh
+++ build.sh
@@ -1,8 +1,20 @@
-cargo build --release && mv target/release/DDPClassicalFix target/release/DDPClassicalFix_Linux_X86_64 &&
-cargo build --release --target i686-unknown-linux-gnu && mv target/i686-unknown-linux-gnu/release/DDPClassicalFix target/i686-unknown-linux-gnu/release/DDPClassicalFix_Linux_i686 &&
-cargo zigbuild --release --target aarch64-unknown-linux-gnu && mv target/aarch64-unknown-linux-gnu/release/DDPClassicalFix target/aarch64-unknown-linux-gnu/release/DDPClassicalFix_Linux_aarch64 &&
-cargo zigbuild --release --target armv7-unknown-linux-gnueabihf && mv target/armv7-unknown-linux-gnueabihf/release/DDPClassicalFix target/armv7-unknown-linux-gnueabihf/release/DDPClassicalFix_Linux_armv7 &&
-cargo build --release --target x86_64-pc-windows-gnu && mv target/x86_64-pc-windows-gnu/release/DDPClassicalFix.exe target/x86_64-pc-windows-gnu/release/DDPClassicalFix_x64.exe &&
+#!/sh
+
+set -e
+
+cargo build --release
+mv target/release/DDPClassicalFix target/release/DDPClassicalFix_Linux_X86_64
+cargo build --release --target i686-unknown-linux-gnu
+mv target/i686-unknown-linux-gnu/release/DDPClassicalFix target/i686-unknown-linux-gnu/release/DDPClassicalFix_Linux_i686
+cargo zigbuild --release --target aarch64-unknown-linux-gnu
+mv target/aarch64-unknown-linux-gnu/release/DDPClassicalFix target/aarch64-unknown-linux-gnu/release/DDPClassicalFix_Linux_aarch64
+cargo zigbuild --release --target armv7-unknown-linux-gnueabihf
+mv target/armv7-unknown-linux-gnueabihf/release/DDPClassicalFix target/armv7-unknown-linux-gnueabihf/release/DDPClassicalFix_Linux_armv7
+cargo build --release --target x86_64-pc-windows-gnu
+mv target/x86_64-pc-windows-gnu/release/DDPClassicalFix.exe target/x86_64-pc-windows-gnu/release/DDPClassicalFix_x64.exe
 cargo build --release --target i686-pc-windows-gnu
-cargo zigbuild --release --target x86_64-apple-darwin && mv target/x86_64-apple-darwin/release/DDPClassicalFix target/x86_64-apple-darwin/release/DDPClassicalFix_Darwin_x86_64 &&
-cargo zigbuild --release --target aarch64-apple-darwin && mv target/aarch64-apple-darwin/release/DDPClassicalFix target/aarch64-apple-darwin/release/DDPClassicalFix_Darwin_aarch64
+mv target/i686-pc-windows-gnu/release/DDPClassicalFix.exe target/i686-pc-windows-gnu/release/DDPClassicalFix_x32.exe
+cargo zigbuild --release --target x86_64-apple-darwin
+mv target/x86_64-apple-darwin/release/DDPClassicalFix target/x86_64-apple-darwin/release/DDPClassicalFix_Darwin_x86_64
+cargo zigbuild --release --target aarch64-apple-darwin
+mv target/aarch64-apple-darwin/release/DDPClassicalFix target/aarch64-apple-darwin/release/DDPClassicalFix_Darwin_aarch64
